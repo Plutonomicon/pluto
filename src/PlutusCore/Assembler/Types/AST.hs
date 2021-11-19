48867dd359757c3fd89361f6a9c19fddd2347370
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-11-19 15:10:10 -0500

    add AST type

---
+++ src/PlutusCore/Assembler/Types/AST.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module PlutusCore.Assembler.Types.AST
+  ( Program (..)
+  , Name (..)
+  , Term (..)
+  , IfTerm (..)
+  , ThenTerm (..)
+  , ElseTerm (..)
+  , LeftTerm (..)
+  , RightTerm (..)
+  , OpTerm (..)
+  , Binding (..)
+  ) where
+
+
+import PlutusCore.Assembler.Prelude
+import PlutusCore.Assembler.Types.Constant (Constant)
+import PlutusCore.Assembler.Types.Builtin (Builtin)
+
+
+newtype Program = Program { unProgram :: Term }
+
+
+newtype Name = Name { getName :: Text }
+
+
+data Term =
+    Var Name
+  | Lambda Binding
+  | Apply Term Term
+  | Force Term
+  | Delay Term
+  | Constant Constant
+  | Builtin Builtin
+  | Error
+  | Let [Binding] Term
+  | IfThenElse IfTerm ThenTerm ElseTerm
+  | InfixApply LeftTerm OpTerm RightTerm
+
+
+newtype IfTerm = IfTerm Term
+
+
+newtype ThenTerm = ThenTerm Term
+
+
+newtype ElseTerm = ElseTerm Term
+
+
+newtype LeftTerm = LeftTerm Term
+
+
+newtype RightTerm = RightTerm Term
+
+
+newtype OpTerm = OpTerm Term
+
+
+data Binding = Binding Name Term
---
+++ src/PlutusCore/Assembler/Types/Constant.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module PlutusCore.Assembler.Types.Constant
+  ( Constant (..)
+  ) where
+
+
+import PlutusCore.Assembler.Prelude
+import PlutusCore.Data (Data)
+
+
+data Constant =
+    I Integer
+  | S ByteString
+  | T Text
+  | U
+  | B Bool
+  | L [Constant]
+  | P (Constant, Constant)
+  | D Data
