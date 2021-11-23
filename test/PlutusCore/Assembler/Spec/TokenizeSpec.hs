f32a691e86ab5f7cce4c458f1d9ddcc71d7f241c
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-11-23 12:37:55 -0500

    add property tests

--- test/PlutusCore/Assembler/Spec/TokenizeSpec.hs
+++ test/PlutusCore/Assembler/Spec/TokenizeSpec.hs
@@ -18,18 +18,40 @@ genText :: Gen Text
 genText = Gen.text (Range.linear 0 1000) Gen.ascii
 
 
+genWhitespace :: Gen Text
+genWhitespace = Gen.text (Range.singleton 1) (Gen.element [' ', '\r', '\n'])
+
+
 tests :: TestTree
 tests =
   testGroup "tokenize"
-  [ commutesWithConcatByNewline
+  [ commutesWithConcatByNewlineTest
+  , prependWhitespaceTest
+  , appendWhitespaceTest
   ]
 
 
-commutesWithConcatByNewline :: TestTree
-commutesWithConcatByNewline =
+commutesWithConcatByNewlineTest :: TestTree
+commutesWithConcatByNewlineTest =
   testProperty "commutes with concatenating strings separated by a newline" . property $ do
     t0 <- forAll genText
     t1 <- forAll genText
     liftA2 (<>) (f t0) (f t1) === f (t0 <> "\n" <> t1)
     return ()
   where f = eitherToMaybe . tokenize
+
+
+prependWhitespaceTest :: TestTree
+prependWhitespaceTest =
+  testProperty "result is unaffected by prepending whitespace" . property $ do
+    t <- forAll genText
+    w <- forAll genWhitespace
+    eitherToMaybe (tokenize t) === eitherToMaybe (tokenize (w <> t))
+
+
+appendWhitespaceTest :: TestTree
+appendWhitespaceTest =
+  testProperty "result is unaffected by appending whitespace" . property $ do
+    t <- forAll genText
+    w <- forAll genWhitespace
+    eitherToMaybe (tokenize t) === eitherToMaybe (tokenize (w <> t))
