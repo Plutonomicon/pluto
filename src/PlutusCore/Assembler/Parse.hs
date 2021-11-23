d924c3cf3c285c3fdd91400aef0880e719e05e32
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-11-23 15:47:56 -0500

    wip parser

--- src/PlutusCore/Assembler/Parse.hs
+++ src/PlutusCore/Assembler/Parse.hs
@@ -30,14 +30,66 @@ term = term0
 
 
 term0 :: Parser Term
-term0 = do
-  t  <- term1
-  ts <- P.many term1
-  return $ foldl Apply t ts
+term0 = lambdaTerm <|> term1
+
+
+lambdaTerm :: Parser Term
+lambdaTerm = todo
 
 
 term1 :: Parser Term
-term1 = todo
+term1 = do
+  t  <- term1
+  ts <- P.many term2
+  return $ foldl Apply t ts
+
+
+term2 :: Parser Term
+term2 = ifTerm <|> letTerm <|> term3
+
+
+ifTerm :: Parser Term
+ifTerm = todo
+
+
+letTerm :: Parser Term
+letTerm = todo
+
+
+term3 :: Parser Term
+term3 = forceTerm <|> delayTerm <|> infixApplyTerm <|> term4
+
+
+forceTerm :: Parser Term
+forceTerm = todo
+
+
+delayTerm :: Parser Term
+delayTerm = todo
+
+
+infixApplyTerm :: Parser Term
+infixApplyTerm = todo
+
+
+term4 :: Parser Term
+term4 = constantTerm <|> builtinTerm <|> errorTerm <|> parenthesizedTerm
+
+
+constantTerm :: Parser Term
+constantTerm = todo
+
+
+builtinTerm :: Parser Term
+builtinTerm = todo
+
+
+errorTerm :: Parser Term
+errorTerm = todo
+
+
+parenthesizedTerm :: Parser Term
+parenthesizedTerm = todo
 
 
 todo :: a
