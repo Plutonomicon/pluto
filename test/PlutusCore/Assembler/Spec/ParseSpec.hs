3898ce8f8ff0601a3a87fa68f335cf1593edbda5
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-11-26 12:05:05 -0500

    wip parser test

--- test/PlutusCore/Assembler/Spec/ParseSpec.hs
+++ test/PlutusCore/Assembler/Spec/ParseSpec.hs
@@ -4,11 +4,24 @@
 module PlutusCore.Assembler.Spec.ParseSpec ( tests ) where
 
 
+import Text.Parsec.Pos (newPos)
+
 import PlutusCore.Assembler.Spec.Prelude
 
 
 tests :: TestTree
 tests =
   testGroup "parse"
-  [
+  [ testParseValidTokenList
   ]
+
+
+fakeSourcePos :: SourcePos
+fakeSourcePos = newPos "test" 0 0
+
+
+testParseValidTokenList :: TestTree
+testParseValidTokenList =
+  testProperty "parses a syntactically valid token list" $ do
+    (t, tts) <- genTerm
+    parse t === Right tts
