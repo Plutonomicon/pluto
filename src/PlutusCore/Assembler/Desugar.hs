540cb28274e0cef3b3e020fb1d88d7eb93e58ec4
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-11-27 00:39:07 -0500

    desugar var

--- src/PlutusCore/Assembler/Desugar.hs
+++ src/PlutusCore/Assembler/Desugar.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 
 module PlutusCore.Assembler.Desugar ( desugar ) where
@@ -9,6 +11,8 @@ import PlutusCore.DeBruijn (DeBruijn)
 import PlutusCore.Default (DefaultUni, DefaultFun)
 import qualified UntypedPlutusCore.Core.Type as UPLC
 import qualified PlutusCore.Core as PLC
+import qualified Data.Map as Map
+import qualified Data.Text as Text
 
 import PlutusCore.Assembler.Prelude
 import PlutusCore.Assembler.Types.AST (Program, Term, Name)
@@ -27,7 +31,13 @@ desugar (AST.Program x) =
 
 desugarTerm :: Term (SourcePos, Map Name DeBruijn)
             -> Either Text UnsweetTerm
-desugarTerm = todo
+desugarTerm =
+  \case
+    AST.Var (p, m) x ->
+      case Map.lookup x m of
+        Just i -> pure (UPLC.Var () i)
+        Nothing -> Left ("undefined variable name " <> AST.getName x <> " at source position " <> Text.pack (show p))
+    _ -> todo
 
 
 todo :: a
